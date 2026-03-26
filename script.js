a43e0b17e66d36147a5e92357c36cbed942f148b
joodami <joodami25@gmail.com>
2026-03-26 11:09:01 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbxBvJua6tTVGLb_Jyru7RCsX0NtFvyEksZQZvWrJFosL973pGo1Q2CDys06FkMd2JJiFg/exec"; 
+const API_URL = "https://script.google.com/macros/s/AKfycbwKrBhXebVSjg0g3VMWAMI4WdGEA5VXIVPhL0pAIKYrdf7FqFDR_DR9clQvlcPHJ5Wvpw/exec"; 
 
 // ฟังก์ชันแสดงการแจ้งเตือนแบบ Toast (สวยงามและไม่ขัดจังหวะ)
 function showNotification(message, isSuccess = true) {
